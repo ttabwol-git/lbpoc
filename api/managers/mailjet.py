af381437f23477c6e0a1ae6d5a793558700934c1
ttabwol-git <ttabwol@gmail.com>
2024-06-05 13:41:37 +0200

    reformated mailjet.py

--- api/managers/mailjet.py
+++ api/managers/mailjet.py
@@ -5,32 +5,23 @@ from mailjet_rest import Client
 class MailJetClient:
     
     def __init__(self):
+        self.client = None
+        
+    async def send_email(self, subject: str, text: str) -> dict | None:
         self.client = Client(
-            auth=(os.environ['MAILJET_API_KEY'], os.environ['MAILJET_API_SECRET']), 
+            auth=(os.environ['MAILJET_API_KEY'], os.environ['MAILJET_API_SECRET']),
             version='v3.1'
         )
-        
-    def send_email(self, subject: str, text: str) -> dict | None:
         data = {
-            'Messages': [
-                {
-                    'From': {
-                        'Email': 'lbpoc-test@proton.me',
-                        'Name': 'LBPOC Email Service'
-                    },
-                    'To': [
-                        {
-                            'Email': os.environ['EMAIL_TO_ADDR'],
-                            'Name': os.environ['EMAIL_TO_NAME']
-                        }
-                    ],
-                    'Subject': subject,
-                    'TextPart': text
-                }
-            ]
+            'Messages': [{
+                'From': {'Email': 'lbpoc-test@proton.me', 'Name': 'LBPOC Email Service'},
+                'To': [
+                    {'Email': os.environ['EMAIL_TO_ADDR'], 'Name': os.environ['EMAIL_TO_NAME']}
+                ],
+                'Subject': subject,
+                'TextPart': text
+            }]
         }
         result = self.client.send.create(data=data)
         if result.status_code == 200:
             return result.json()
-        else:
-            return None
